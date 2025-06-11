37fb6b0dc258ac6db4843c3ab4ebdc1268727bc0
范瑞庆 <fanruiqing@xingshicloud.ad>
2025-06-11 16:35:14 +0800

    1、增加启动时的控制台打印日志 2、编译版本控制在17

--- src/main/java/com/ally/learn/springailearning/SpringaiLearningApplication.java
+++ src/main/java/com/ally/learn/springailearning/SpringaiLearningApplication.java
@@ -1,13 +1,46 @@
 package com.ally.learn.springailearning;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@Slf4j
 public class SpringaiLearningApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringaiLearningApplication.class, args);
-    }
+	public static void main(String[] args) throws UnknownHostException {
+
+		ConfigurableEnvironment env = SpringApplication.run(SpringaiLearningApplication.class, args).getEnvironment();
+		String protocol = "http";
+		String context = env.getProperty("server.servlet.context-path");
+		context = context == null ? "" : context;
+		System.out.println("-------------OK---------------");
+		log.info("\n----------------------------------------------------------\n\t" +
+				"Application '{}' is running! Access URLs:\n\t" +
+				"Local: \t\t{}://localhost:{}\n\t" +
+				"External: \t{}://{}:{}\n\t" +
+				"swagger: \t{}://{}:{}/doc.html\n\t" +
+				"ApiDocs: \t{}\n\t" +
+				"active: \t{}\n\t" +
+				"ContextPath: \t{}\n" +
+				"----------------------------------------------------------",
+			env.getProperty("spring.application.name"),
+			protocol,
+			env.getProperty("server.port") + context,
+			protocol,
+			InetAddress.getLocalHost().getHostAddress(),
+			env.getProperty("server.port") + context,
+			protocol,
+			InetAddress.getLocalHost().getHostAddress(),
+			env.getProperty("server.port") + context,
+			env.getProperty("application.doc"),
+			env.getProperty("spring.profiles.active"),
+			context);
+
+
+	}
 
 }
